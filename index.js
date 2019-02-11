e70c8cd01648dabf8b9ee6cbfd16e36e633e11c1
nycrera <alperenyurdakul7@gmail.com>
2019-02-11 15:42:23 +0300

    fix typo and add some checks

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 /* jshint node: true */
 /*jshint esversion: 6 */
-var streamer = function (configuration,callback) {
+var streamer = function (configuration, callback) {
    var generator = require('./conf_generator.js');
-   const {spawn} = require('child_process');
+   const {
+      spawn
+   } = require('child_process');
 
    var fs = require('fs');
    if (typeof (configuration) === 'string') {
       generator.read(configuration, function (data) {
          this.configuration = data;
       });
-   } else if(typeof(configuration) == 'object'){
+   } else if (typeof (configuration) === 'object') {
       this.configuration = configuration;
-      if (!configurtion.filename) {
+      if (!configuration.filename) {
          console.log('[WARN]No playlist specified so creating a new one.'); // If no playlist is specified let us create ourselves
-         configuration.filename = __dirname+'/playlist.m3u';
+         configuration.filename = __dirname + '/playlist.m3u';
          fs.appendFile(configuration.filename, '#EXTM3U\n', function (err) {
             if (err) throw err;
             console.log('[INFO]New playlist file: ' + configuration.filename + ' generated in local directory.');
          });
       }
-      if(callback)
-      generator.create('ezstream-conf.xml', configuration,callback); // Hard coded for now
+      if (typeof (callback) === 'function')
+         generator.create('ezstream-conf.xml', configuration, callback); // Hard coded for now
       else generator.create('ezstream-conf.xml', configuration);
-   }else{
-      throw "icy-streamer constructor only takes either an object or a string! But yours is "+typeof(configuration);
+   } else {
+      throw "icy-streamer constructor only takes either an object or a string! But yours is " + typeof (configuration);
    }
    this.running = false;
    var self = this;
 
-   this.addSong = function (filename,callback) {
+   this.addSong = function (filename, callback) {
       if (!self.running) throw "Can't add new song \"" + filename + "\" while stream is not running.";
       if (!fs.existsSync(filename)) throw "No file exists with this path : " + filename;
       fs.appendFile(self.filename, filename + "\n", function (err) {
          if (err) throw err;
          console.log('[INFO]New song: ' + filename + ' added to playlist.');
          self.flushPlayList();
-         if(callback) callback();
+         if (typeof (callback) === 'function') callback();
       });
    };
 
@@ -50,15 +52,15 @@ var streamer = function (configuration,callback) {
       self.Stream = null;
    };
    this.startStream = function () {
-     try{
-      self.Stream = spawn('ezstream', ['-c', self.configuration.filename]);
-      self.Stream.on("exit",function(){
-         self.Stream.running = false;
-      });
-      self.Stream.running = true;
-   }catch(error){
-       throw error;
-   }
+      try {
+         self.Stream = spawn('ezstream', ['-c', self.configuration.filename]);
+         self.Stream.on("exit", function () {
+            self.Stream.running = false;
+         });
+         self.Stream.running = true;
+      } catch (error) {
+         throw error;
+      }
    };
 
 };
